91679dbf6ef540c542d542099372fab23285fc99
Jared Wilber <j.wilber92@gmail.com>
2016-10-25 13:26:07 -0700

    Updated pls script [jared]'

--- code/scripts/partial_least_squares.R
+++ code/scripts/partial_least_squares.R
@@ -4,7 +4,7 @@
 # This script computes the pls regression of
 # the credit data.
 # -------------------------------------------
-
+# -------------------------------------------
 
 
 
@@ -35,7 +35,6 @@ credit_test  <- scaled_credit[-train_indices,]
 
 
 # Fit Model
-set.seed(1)
 pls.fit=plsr(Balance ~ ., 
              data = data.frame(credit_train),
              scale = FALSE,
@@ -45,9 +44,14 @@ summary(pls.fit)
 
 # Find number of components with best fit
 best_mod_comp <- which.min(pls.fit$validation$PRESS) # 5
+save(best_mod_comp, "data/pls_best_model_component.RData")
 
 # Plot
+
+png(file = "images/pls_validation_plot.png")
 validationplot(pls.fit, val.type = "MSEP", col='skyblue1')
+dev.off()
+message("File Save Successful: ", "pls_validation_plot.png")
 
 # Predict on test set using 5 components
 pls.pred = predict(pls.fit, 
@@ -55,15 +59,17 @@ pls.pred = predict(pls.fit,
                    ncomp = best_mod_comp)
 # MSE
 pls_mse <- mean((pls.pred - credit_test[,12])^2)
+save(pls_mse, "data/pls_mse.RData")
 
 # Refit on full dataset
 pls.fit.full <- plsr(Balance ~ .,
                      data = data.frame(scaled_credit),
                      scale = FALSE, 
                      ncomp = 5)
-summary(pls.fit.full)
+pls_sum_full <- summary(pls.fit.full)
+save(pls_sum_full, file="pls_full_summary.RData")
 
-
+message("PLS Script Finished Running")
 
 
 
