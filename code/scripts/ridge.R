b0bd03765355c356cfd8580f8671f7d95929c0ed
Manuel Horta <manuelhorta45@gmail.com>
2016-10-26 19:29:44 -0700

    ridge script

---
+++ code/scripts/ridge.R
@@ -0,0 +1,12 @@
+
+# -------------------------------------------
+# -------------------------------------------
+#           Ridge Regression
+# This script computes the ridge regression of
+# the credit data.
+# -------------------------------------------
+# -------------------------------------------
+
+source("code/scripts/data-preprocess-man.R")
+# install.packages('glmnet')
+library(glmnet)
