5d80fd3a9fc5877d39c471296159a52f6ff20fac
Jared Wilber <j.wilber92@gmail.com>
2016-11-01 23:14:33 -0700

    Added sink to pls instead of multiple save commands

--- code/scripts/pls_regression.R
+++ code/scripts/pls_regression.R
@@ -8,56 +8,67 @@
 
 message("Begin PLS Script")
 library(pls)
-
+set.seed(1)
 source("code/scripts/data-preprocess.R")
 
 # -------------------------------------------
 # Fit Model
 # -------------------------------------------
-pls.fit=plsr(Balance ~ ., 
+pls_fit=plsr(Balance ~ ., 
              data = data.frame(credit_train),
              scale = FALSE,
              validation = "CV")
-summary(pls.fit)
+summary(pls_fit)
 
 # -------------------------------------------
 # Find number of components with best fit
 # -------------------------------------------
-best_mod_comp <- which.min(pls.fit$validation$PRESS) # 5
-save(best_mod_comp, file = "data/pls_best_model_component.RData")
+best_mod_comp_pls <- which.min(pls_fit$validation$PRESS) # 5
+#save(best_mod_comp, file = "data/pls_best_model_component.RData")
 
 # -------------------------------------------
 # Plot
 # -------------------------------------------
 png(file = "images/pls_validation_plot.png")
-validationplot(pls.fit, val.type = "MSEP", col='skyblue1')
+validationplot(pls_fit, val.type = "MSEP", col='skyblue1')
 dev.off()
 message("File Save Successful: ", "pls_validation_plot.png")
 
 # -------------------------------------------
-# Predict on test set using 5 components
+# Predict and MSE
 # -------------------------------------------
-pls.pred = predict(pls.fit, 
+pls_pred = predict(pls_fit, 
                    credit_test[,-12],
-                   ncomp = best_mod_comp)
+                   ncomp = best_mod_comp_pls)
 
-# -------------------------------------------
-# MSE
-# -------------------------------------------
-pls_mse <- mean((pls.pred - credit_test[,12])^2)
-save(pls_mse, file = "data/pls_mse.RData")
+
+pls_mse <- mean((pls_pred - credit_test[,12])^2)
+#save(pls_mse, file = "data/pls_mse.RData")
 
 # -------------------------------------------
 # Refit on full dataset
 # -------------------------------------------
-pls.fit.full <- plsr(Balance ~ .,
+pls_fit_full <- plsr(Balance ~ .,
                      data = data.frame(scaled_credit),
                      scale = FALSE, 
                      ncomp = 5)
-pls_sum_full <- summary(pls.fit.full)
-save(pls_sum_full, file="pls_full_summary.RData")
+
+pls_full_coefs <- coefficients(pls_fit_full)
+#save(pls_sum_full, file="pls_full_summary.RData")
+
+save(pls_fit, best_mod_comp_pls, pls_pred, pls_mse, pls_full_coefs, 
+     file ='data/pls-saved-objects.Rdata')
+
+
+sink('data/model-results/pls-results.txt')
+'> best_mod_comp_pls' 
+best_mod_comp_pls
+'> pls test MSE'
+pls_mse
+'> pls official coefficients'
+pls_full_coefs
+sink(NULL)
+
 
 message("PLS Script Finished Running")
 # end
-
-
