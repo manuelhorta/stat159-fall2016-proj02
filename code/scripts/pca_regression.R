7672d153e15acbeba8ba9bfb93626066eb742c10
Jared Wilber <j.wilber92@gmail.com>
2016-11-01 22:21:32 -0700

    Updated pca regression by using sink instead of save

--- code/scripts/pca_regression.R
+++ code/scripts/pca_regression.R
@@ -8,61 +8,69 @@
 
 message("Running pca script")
 library(pls)
-
+set.seed(1)
 source("code/scripts/data-preprocess.R")
 
 # -------------------------------------------
 # Fit Model
 # -------------------------------------------
-pcr.fit=pcr(Balance ~ ., 
+pcr_fit=pcr(Balance ~ ., 
              data = data.frame(credit_train),
              scale = FALSE,
              validation = "CV")
-summary(pcr.fit)
+summary(pcr_fit)
 
 
 
 # -------------------------------------------
 # Find number of components with best fit
 # -------------------------------------------
-best_mod_comp_pcr <- which.min(pcr.fit$validation$PRESS) # 10
-save(best_mod_comp_pcr, file = "data/pcr_best_model_component.RData")
+best_mod_comp_pcr <- which.min(pcr_fit$validation$PRESS) # 10
+#save(best_mod_comp_pcr, file = "data/pcr_best_model_component.RData")
 
 # -------------------------------------------
 # Plot
 # -------------------------------------------
 png(file = "images/pcr_validation_plot.png")
-validationplot(pcr.fit, val.type = "MSEP", col='skyblue1')
+validationplot(pcr_fit, val.type = "MSEP", col='skyblue1')
 dev.off()
 message("File Save Successful: ", "pcr_validation_plot.png")
 
 # -------------------------------------------
-# Predict on test set using 5 components
+# Predict and MSE
 # -------------------------------------------
-pcr.pred = predict(pcr.fit, 
+pcr_pred = predict(pcr_fit, 
                    credit_test[,-12],
                    ncomp = best_mod_comp_pcr)
 
-# -------------------------------------------
-# MSE
-# -------------------------------------------
-pcr_mse <- mean((pcr.pred - credit_test[,12])^2)
-save(pcr_mse, file = "data/pcr_mse.RData")
+pcr_mse <- mean((pcr_pred - credit_test[,12])^2)
+#save(pcr_mse, file = "data/pcr_mse.RData")
 
 # -------------------------------------------
-# Refit on full dataset
+# Refit on full dataset & get coefficients
 # -------------------------------------------
-pcr.fit.full <- pcr(Balance ~ .,
+pcr_fit_full <- pcr(Balance ~ .,
                      data = data.frame(scaled_credit),
                      scale = FALSE, 
                      ncomp = best_mod_comp_pcr)
-pcr_sum_full <- summary(pcr.fit.full)
-save(pcr_sum_full, file = "pcr_full_summary.RData")
+
+#save(pcr_sum_full, file = "pcr_full_summary.RData")
+pcr_full_coefs <- coefficients(pcr_fit_full)
 
 message("PCR Script Finished Running")
 
 
+save(pcr_fit, best_mod_comp_pcr, pcr_pred, pcr_mse, pcr_full_coefs, 
+     file ='data/pcr-saved-objects.Rdata')
 
 
+sink('data/model-results/pcr-results.txt')
+'> best_mod_comp_pcr' 
+best_mod_comp_pcr
+'> pcr test MSE'
+pcr_mse
+'> pcr official coefficients'
+pcr_full_coefs
+sink(NULL)
 
 
