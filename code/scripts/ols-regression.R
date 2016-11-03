21948c609e6d6bc69a164727c60028b082eda8ed
Austin Carango <austincarango@gmail.com>
2016-11-02 17:44:39 -0700

    added ols script, data, and plots

---
+++ code/scripts/ols-regression.R
@@ -0,0 +1,40 @@
+# -------------------------------------------
+# -------------------------------------------
+#           OLS Regression
+# This script computes the OLS regression of
+# the credit data.
+# -------------------------------------------
+# -------------------------------------------
+
+message("Running OLS script")
+source("code/scripts/data-preprocess.R")
+
+# -------------------------------------------
+# Fit Model
+# -------------------------------------------
+
+ols<-lm(Balance~Income+Limit+Rating+Cards+Age+Education
+        +GenderFemale+StudentYes+MarriedYes+EthnicityAsian
+        +EthnicityCaucasian, 
+        data=as.data.frame(scaled_credit))
+
+summary_ols <- summary(ols)
+
+sink(file = "data/model-results/ols-results.txt")
+summary_ols
+sink()
+
+save(ols, summary_ols, 
+     file ='data/ols-saved-objects.Rdata')
+
+# -------------------------------------------
+# Plot
+# -------------------------------------------
+
+png(file = "images/ols_plots.png")
+par(mfrow=c(2,2))
+plot(ols)
+dev.off()
+
+message("OLS Script Finished Running")
+
